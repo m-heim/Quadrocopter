4bf2af87d8293b39ee4e4a23d6ea97628b57ef05
m-heim <maximilianheim@protonmail.com>
2026-05-08 17:40:55 +0200

    Update App

--- include/mcapp.hpp
+++ include/mcapp.hpp
@@ -1,6 +1,7 @@
 #ifndef MCAPP_HPP_
 #define MCAPP_HPP_
 #define MCAPP_VERSION 1
+#define NO_PIN -1
 #include "init.hpp"
 #include "utils.hpp"
 #include "Arduino.h"
@@ -16,7 +17,7 @@ public:
     MCApp(CommunicationProvider *remote) : remote(remote)
     {
     }
-    inline void output(int start, int stop, int step, float seconds)
+    void output(int start, int stop, int step, float seconds)
     {
         int s = (int)((seconds / 1000));
         for (int i = start; i <= stop; i += step)
@@ -30,107 +31,13 @@ public:
         return this->remote;
     }
 
-    inline bool recentMessage()
+    bool recentMessage()
     {
         return (millis() - msg_a) < NO_MSG;
     }
-    inline bool handle()
-    {
-        bool valid = false;
-        if (remote->read() > 0)
-        { // is there a payload? get the pipe number that recieved it
-            valid = true;
-            uint8_t *buf = remote->getBuf();
-            if (buf[0] == HELLO)
-            {
-                isConnected = true;
-                log("Hello from sender");
-                output(500, 900, 100, 0.04);
-            }
-            else if (buf[0] == BYE)
-            {
-                isConnected = false;
-                log("Bye from sender");
-                output(900, 500, -100, 0.04);
-            }
-            else if (buf[0] == GYRO_SETUP)
-            {
-                log("Received gyro setup");
-                // setGravity();
-            }
-            else if (buf[0] == MOTOR_SETUP)
-            {
-                log("Received motor setup");
-            }
-            else if (buf[0] == CONTROL)
-            {
-                log("Received payload");
-                memcpy(&payload, buf + 2, sizeof(payload));
-                printPayload(payload);
-                msg_a = millis();
-            }
-            else
-            {
-                log("Invalid message");
-                valid = false;
-            }
-        }
-        else
-        {
-            log("Radio not available");
-        }
-        if (!recentMessage())
-        {
-            noPackageAction();
-            valid = false;
-            isConnected = false;
-        }
-        else
-        {
-        }
-        return valid;
-    }
+    bool handle();
 
-    inline bool handle2(int8_t *buf, bool gyroSetup)
-    {
-        int payloadLength = 0;
-        int action = 1;
-        if (!isConnected)
-        {
-            msgBuf[0] = HELLO;
-            msgBuf[1] = 0;
-            payloadLength = 2;
-            log("Sending hello");
-        }
-        else if (action == 1)
-        {
-            msgBuf[0] = CONTROL;
-            msgBuf[1] = 4;
-            payloadLength = 2 + sizeof(ReceiverPayload);
-            payload.speed = buf[0];
-            payload.pitch = buf[1];
-            payload.yaw = buf[2];
-            payload.roll = buf[3];
-            memcpy(msgBuf + 2, &payload, sizeof(payload));
-            log("Sending");
-        }
-        else
-        {
-            log("Unknown action");
-        }
-        bool report = remote->write(msgBuf, payloadLength);
-        if (report)
-        {
-            log("Message was successfully transmitted");
-            isConnected = true;
-        }
-        else
-        {
-            log("No ack from receiver");
-            isConnected = false;
-        }
-        return isConnected;
-    }
+    bool handle2(int8_t *buf, bool gyroSetup);
 
     void noPackageAction()
     {
@@ -143,11 +50,11 @@ public:
             freq = FREQ_BASE;
         }
     }
-    inline int getVersion()
+    int getVersion()
     {
         return MCAPP_VERSION;
     }
-    inline void printPayload(ReceiverPayload p)
+    void printPayload(ReceiverPayload p)
     {
         if (Serial)
         {
@@ -159,7 +66,7 @@ public:
             Serial.print("\n");
         }
     }
-    inline float getVoltage()
+    float getVoltage()
     {
         if (voltagePin == -1)
         {
@@ -170,31 +77,33 @@ public:
             return (analogRead(voltagePin) / 1024) * voltageFactor;
         }
     }
-    inline bool verifyVoltage()
+    bool verifyVoltage()
     {
-        return (voltagePin == -1) || (getVoltage() > voltage);
+        return (voltagePin == NO_PIN) || (getVoltage() > voltage);
     }
-    inline void initPiezo(int pin)
+    void initPiezo(int pin)
     {
         pinMode(pin, OUTPUT);
         piezoPin = pin;
     }
-    inline void initVoltage(int pin, float factor, float v)
+    void initVoltage(int pin, float factor, float v)
     {
         pinMode(pin, INPUT);
         voltagePin = pin;
         voltageFactor = factor;
         voltage = v;
     }
-    inline void buzz(int freq, int dur)
+    // buzz if possible
+    void buzz(int freq, int dur)
     {
-        if (piezoPin == -1)
+        if (piezoPin == NO_PIN)
         {
             return;
         }
         tone(piezoPin, freq, dur);
     }
-    inline void ledError()
+    // infinite loop led error
+    void ledError()
     {
         while (1)
         {
@@ -204,15 +113,18 @@ public:
             delay(1000);
         }
     }
-    inline void initLog(long baud)
+    // init logger
+    void initLog(long baud)
     {
+#if DEBUG == 1
         if (Serial)
         {
             Serial.begin(baud);
             Serial.println("Serial init");
         }
+#endif
     }
-    inline void log(char *msg)
+    void log(char *msg)
     {
 #if DEBUG == 1
         if (Serial)
@@ -221,20 +133,20 @@ public:
         }
 #endif
     }
-    inline void initLed(int pin)
+    void initLed(int pin)
     {
         pinMode(pin, OUTPUT);
         this->ledPin = pin;
     }
-    inline void infinite()
+    void infinite()
     {
         while (1)
         {
         }
     }
-    inline void setLed(int val)
+    void setLed(int val)
     {
-        if (this->ledPin == -1)
+        if (this->ledPin == NO_PIN)
         {
             return;
         }
@@ -243,7 +155,8 @@ public:
             digitalWrite(ledPin, val);
         }
     }
-    inline void ledToggle()
+    // toggle led state and set
+    void ledToggle()
     {
         this->setLed(ledState);
         ledState = !ledState;
@@ -256,17 +169,16 @@ public:
     }
 
 private:
-    int voltagePin = -1;
-    int piezoPin = -1;
-    int ledPin = -1;
-    float voltageFactor = 1;
-    float voltage = 0;
+    int voltagePin = NO_PIN;
+    int piezoPin = NO_PIN;
+    int ledPin = NO_PIN;
+    float voltageFactor = 1.0;
+    float voltage;
     bool ledState = false;
     int freq = FREQ_BASE;
-    // last control packet
-    long msg_a;
-    bool isConnected = false;
-    ReceiverPayload payload = {0, 0, 0, 0};
+    long msg_a; // last control packet
+    bool isConnected = false; // connection status
+    ReceiverPayload payload = {0, 0, 0, 0}; // current payload
     uint8_t msgBuf[PAYLOAD_LENGTH];
 };
 #endif
--- src/mcapp.cpp
+++ src/mcapp.cpp
@@ -1,1 +1,91 @@
 #include "mcapp.hpp"
+
+    bool MCApp::handle()
+    {
+        bool valid = false;
+        if (remote->read() > 0)
+        { // is there a payload? get the pipe number that recieved it
+            valid = true;
+            const uint8_t *buf = remote->getBuf();
+            // setup gyro
+            if (buf[0] == GYRO_SETUP)
+            {
+                log("Received gyro setup");
+                // setGravity();
+            }
+            // setup motor
+            else if (buf[0] == MOTOR_SETUP)
+            {
+                log("Received motor setup");
+            }
+            // control message
+            else if (buf[0] == CONTROL)
+            {
+                log("Received payload");
+                memcpy(&payload, buf + 2, sizeof(payload));
+                printPayload(payload);
+                msg_a = millis();
+            }
+            else
+            {
+                log("Invalid message");
+                valid = false;
+            }
+        }
+        else
+        {
+            log("Radio not available");
+        }
+        if (!recentMessage())
+        {
+            noPackageAction();
+            valid = false;
+        }
+        else
+        {
+        }
+        return valid;
+    }
+
+bool MCApp::handle2(int8_t *buf, bool gyroSetup)
+    {
+        int payloadLength = 0;
+        int action = 1;
+        if (gyroSetup) {
+            msgBuf[0] = GYRO_SETUP;
+            msgBuf[1] = 0;
+            payloadLength = 2;
+            log("Sending setup");
+        }
+        else if (action == 1)
+        {
+            msgBuf[0] = CONTROL;
+            msgBuf[1] = 4;
+            payloadLength = 2 + sizeof(ReceiverPayload);
+            payload.speed = buf[0];
+            payload.pitch = buf[1];
+            payload.yaw = buf[2];
+            payload.roll = buf[3];
+            memcpy(msgBuf + 2, &payload, sizeof(payload));
+            log("Sending");
+        }
+        else
+        {
+            log("Unknown action");
+        }
+        bool report = remote->write(msgBuf, payloadLength);
+        if (report)
+        {
+            log("Message was successfully transmitted");
+            msg_a = millis();
+        }
+        else
+        {
+            log("No ack from receiver");
+        }
+        if (!recentMessage())
+        {
+            log("No recent message");
+        }
+        return report;
+    }
